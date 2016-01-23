8174ec90e85eba469103f4be193fe0866f82bb01
Mitch Thompson <thomitchell@gmail.com>
2016-01-23 12:00:58 -0800

    Classic ADO assignment

---
+++ ClassicADO/App_Code/BookReviews.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+using System.Data;
+using System.Data.SqlClient;
+using System.Configuration;
+
+public class BookReviews
+{
+    SqlConnection connect;
+	public BookReviews()
+	{
+
+        connect = new SqlConnection(ConfigurationManager.
+            ConnectionStrings["BookReviewConnectionString"].ToString());
+	}
+
+    public DataTable GetAuthors()
+    {
+        string sql = "SELECT authorkey, authorname FROM Author";
+        DataTable tbl;
+
+        SqlCommand cmd = new SqlCommand(sql, connect);
+        try
+        {
+         
+            tbl = ProcessQuery(cmd);
+        }
+        catch(Exception ex)
+        {
+            
+            throw ex;
+        }
+        return tbl;
+        
+    }
+    public DataTable GetBooks(int authorKey)
+    {
+        string sql = "SELECT * FROM book INNER JOIN authorbook ON book.bookkey= authorbook.bookkey WHERE authorkey = @authorkey";
+        SqlCommand cmd = new SqlCommand(sql, connect);
+        
+        cmd.Parameters.Add("@authorkey", authorKey);
+
+        DataTable tbl;
+        try
+        {
+            tbl = ProcessQuery(cmd);
+        }
+        catch (Exception ex)
+        {
+            throw ex;
+        }
+        return tbl;
+
+    }
+
+    private DataTable ProcessQuery(SqlCommand cmd)
+    {
+        DataTable table = new DataTable();
+        SqlDataReader reader;
+        try
+        {
+            connect.Open();
+            reader = cmd.ExecuteReader();
+            table.Load(reader);
+            connect.Close();
+        }
+        catch (Exception ex)
+        {
+            throw ex;
+        }
+        return table;
+    }
+}
---
+++ ClassicADO/Default.aspx
@@ -0,0 +1,35 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="Default.aspx.cs" Inherits="_Default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+    <link href="ClassicADO.css" rel="stylesheet" type="text/css" />
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+        <h1>Authors</h1>
+        <hr />
+        <p>Select an author from the list</p>
+
+
+        <asp:DropDownList 
+            ID="AuthorDropDownList" 
+            runat="server" 
+            AutoPostBack="true" 
+            OnSelectedIndexChanged="AuthorDropDownList_SelectedIndexChanged"
+            CssClass="list">
+        </asp:DropDownList>
+
+        <asp:GridView 
+            ID="BooksGridView" 
+            runat="server">
+        </asp:GridView>
+        
+        <asp:Label ID="ErrorLabel" runat="server" Text=""></asp:Label>
+    </div>
+    </form>
+</body>
+</html>
