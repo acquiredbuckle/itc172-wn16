0bd2bf94d0e99b3d16cac51038258f75b03b9315
Mitch Thompson <thomitchell@gmail.com>
2016-02-29 10:42:00 -0800

    creates select web client

---
+++ SelectWebClient/Default.aspx
@@ -0,0 +1,27 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="Default.aspx.cs" Inherits="_Default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+        <h1>Choose a Venue by Name</h1>
+        <asp:DropDownList ID="VenueDropDownList" runat="server"></asp:DropDownList>
+        <asp:Button ID="VenueButton" runat="server" Text="Get Venues" Onclick="VenueButton_Click"/>
+        <asp:DataList ID="VenueDataList" runat="server"></asp:DataList>
+        <h1>Choose a Artist by Name</h1>
+        <asp:DropDownList ID="ArtistDropDownList" runat="server"></asp:DropDownList>
+        <asp:Button ID="ArtistButton" runat="server" Text="Button" OnClick="ArtistButton_Click" />
+        <asp:DataList ID="ArtistDataList" runat="server"></asp:DataList>
+        <h1>Select a Show</h1>
+        <asp:DropDownList ID="ShowDropDownList" runat="server"></asp:DropDownList>
+        <asp:Button ID="ShowButton" runat="server" Text="Button" OnClick="ShowButton_Click" />
+        <asp:DataList ID="ShowDataList" runat="server"></asp:DataList>
+    </div>
+    </form>
+</body>
+</html>
---
+++ SelectWebClient/Default.aspx.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class _Default : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+
+    }
+
+    protected void VenueButton_Click(object sender, EventArgs e)
+    {
+        GetVenues();
+    }
+    protected String GetVenues()
+    {
+        ServiceReference.ShowTrackerServiceClient() stc = new ServiceReference.ShowTrackerServiceClient();
+   
+      
+        return GetVenues();
+        
+    }
+    protected void ArtistButton_Click(object sender, EventArgs e)
+    {
+        stc.GetArtists();
+    }
+    protected String GetVenues()
+    {
+        ServiceReference.ShowTrackerServiceClient() stc = new ServiceReference.ShowTrackerServiceClient();
+   
+      
+        return stc.GetArtists();
+        
+    }
+    protected void ShowsButton_Click(object sender, EventArgs e)
+    {
+        GetShows();
+    }
+    protected String GetShows()
+    {
+        ServiceReference.ShowTrackerServiceClient() stc = new ServiceReference.ShowTrackerServiceClient();
+   
+      
+        return stc.GetShows();
+        
+    }
+
+
+
+}
